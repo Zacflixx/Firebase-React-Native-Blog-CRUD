ea6c0e15c74caaaf4e7390844d3bc88d8d0611bd
Isaac B. Conteh <67395857+Zacflixx@users.noreply.github.com>
2021-02-10 03:26:53 +0800

    Fixed Route Error

---
+++ !st Practice/Refirebase/components/Edit.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+class Edit extends Component {
+  render() {
+    return (
+      <View style={Styles.container}>
+        <Text>I am Edit screen </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
+
+export default Edit;
